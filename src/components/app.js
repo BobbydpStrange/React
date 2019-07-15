c3c6ee4f8df375f255b24fa8d91a80f82929a3ab
BoobbydpStrange <bobbydpblue@gmail.com>
2019-07-15 12:01:29 -0600

    sixth commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,18 @@
 // Imports the `React` module and the `Component` class from that module.
 import React, { Component } from 'react';
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
-import PortfolioContainer from './portfolio/portfolio-container';
+
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
 
 // Creates an `App` class that is a child of the `Component` class.
 class App extends Component {
@@ -10,9 +20,21 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Aubrey Oblads Portfolio</h1>
+        <Router>
+          <div>
+          <h1>Aubrey Oblads Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-        <PortfolioContainer />
+          <NavigationContainer />
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about-me" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/blog" component={Blog} />
+          </Switch>
+          </div>
+        </Router>
+        
       </div>
 
     );
