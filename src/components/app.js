6b0ac2ee902d4748813a7232e4301f8b77b1fdb0
BobbydpStrange <51657838+BobbydpStrange@users.noreply.github.com>
2019-07-15 00:08:25 -0600

    fifth commit

--- src/components/app.js
+++ src/components/app.js
@@ -2,16 +2,17 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 // Creates an `App` class that is a child of the `Component` class.
 class App extends Component {
 
   render() {
-    
     return (
-
       <div className='app'>
         <h1>Aubrey Oblads Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
       </div>
 
     );
