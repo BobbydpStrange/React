ecf9f4ba528b2db2615e4b5cdddd96496b2d8205
BoobbydpStrange <bobbydpblue@gmail.com>
2019-07-17 10:45:55 -0600

    eleventh commit

--- src/components/app.js
+++ src/components/app.js
@@ -13,6 +13,7 @@ import About from './pages/about';
 import Contact from './pages/contact';
 import Blog from './pages/blog';
 import PortfolioDetail from "./portfolio/portfolio-detail.js"
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 // Creates an `App` class that is a child of the `Component` class.
@@ -26,6 +27,7 @@ class App extends Component {
 
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/auth" component={Auth} />
             <Route path="/about-me" component={About} />
             <Route path="/contact" component={Contact} />
             <Route path="/blog" component={Blog} />
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -58,21 +58,19 @@ export default class PortfolioContainer extends Component {
 
 
        return (
-        <div>
-            <h2>{this.state.pageTitle}</h2>
-
-            <button onClick={() => this.handleFilter('eCommerce')}>
-                eCommerce
+            <div className="portfolio-items-wrapper">
+                <button className="btn" onClick={() => this.handleFilter('eCommerce')}>
+                    eCommerce
                 </button>
-            <button onClick={() => this.handleFilter('Scheduling')}>
-                Scheduling
+                <button className="btn" onClick={() => this.handleFilter('Scheduling')}>
+                    Scheduling
                 </button>
-            <button onClick={() => this.handleFilter('Enterprise')}>
-                Enterprise
+                <button className="btn" onClick={() => this.handleFilter('Enterprise')}>
+                    Enterprise
                 </button>
 
-            <div className="portfolio-items-wrapper">{this.portfolioItems()} </div>
-        </div>
+                {this.portfolioItems()} 
+            </div>
        );
     }
 }
