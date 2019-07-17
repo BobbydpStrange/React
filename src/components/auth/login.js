a2dc865fbe287ca886749fdf5ba4625addc9d382
BoobbydpStrange <bobbydpblue@gmail.com>
2019-07-17 11:25:19 -0600

    twelve commit

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,7 +7,8 @@ export default class Login extends Component {
         
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            errorText: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -17,7 +18,8 @@ export default class Login extends Component {
 
     handleChange(event) {
        this.setState({
-           [event.target.name]: event.target.value
+           [event.target.name]: event.target.value,
+           errorText: ""
        });
     }
 
@@ -32,8 +34,18 @@ export default class Login extends Component {
         { withCredentials: true }
         )
         .then(response => {
-            console.log("response", response);
-        })
+            if (response.data.status === 'created') {
+                console.log("You can come in...")
+            } else {
+                this.setState({
+                    errorText: "Wrong email or password"
+                });
+            }
+        }).catch(error => {
+            this.setState({
+                errorText: "An error occurred"
+            })
+        });
 
         event.preventDefault();
     }
@@ -42,6 +54,8 @@ export default class Login extends Component {
         return (
             <div>
             <h1> LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+            <div>{this.state.errorText}</div> 
 
             <h2>{this.state.email}</h2>
             <h2>{this.state.password}</h2>
